c4cf59288b86e863d8c809a1ace7c780745c5ee8
duykhanh3612 <113409523+duykhanh3612@users.noreply.github.com>
2024-08-15 02:04:43 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weather App</title>
-    <link rel="stylesheet" href="{{ secure_asset('asset('/css/app.css') }}"rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+
 </head>
 <body>
     <div class="container">
