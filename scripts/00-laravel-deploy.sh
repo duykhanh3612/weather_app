0b2ed5645d99db859c0f8af44aa30cf90b780872
duykhanh3612 <113409523+duykhanh3612@users.noreply.github.com>
2024-08-15 21:24:38 +0700

    update queue artisan

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,7 @@ echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
 
-
+ 
 echo "Caching config..."
 php artisan config:cache
 
@@ -13,3 +13,5 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force 
 
+echo "Starting queue worker..."
+php artisan queue:work --sleep=3 --tries=3 --daemon --working-dir=/var/www/html
